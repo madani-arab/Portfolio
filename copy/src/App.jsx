0d0d1a14a0219724df80f382b0f0f8abbbcb2250
Abdirahman <m14838842@gmail.com>
2025-10-04 14:33:54 +0300

    Initial commit

---
+++ copy/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+
+
+const App = () =>{
+    return (
+        <>
+       
+        <Header />
+        <main> 
+            <Hero/>
+        </main>
+ </>
+       
+    )
+    
+}
+export default App;
